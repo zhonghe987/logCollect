e77027fbc3c8cdb311b97884cf8c9c2f09208b81
zhonghe987 <zhonghe987@163.com>
2018-06-14 17:50:41 +0800

    update the way that pull log

--- src/main/logCollect.go
+++ src/main/logCollect.go
@@ -8,6 +8,7 @@ import (
     "strings"
     "time"
     "bytes"
+    "runtime"
 )
 
 func cmdExec(cmd string) string {
@@ -27,39 +28,52 @@ func cmdExec(cmd string) string {
      
 }
 
+func memcli(log chan string){
+     for {
+     fmt.Printf("---%s",<-log)
+     }
+}
 
-func pullLog(cmd string){
-     date := time.Now()
-     minute := int(date.Minute())
-     fmt.Println(minute) 
-     if (minute > 20) {
-         fmt.Printf("-------")
-     }else{
-       for {
-         
-         select{
-            default:
-                now_date := time.Now()
-                hour := string(now_date.Hour())
-                //dateString := now_date.Format("2006-01-02")
-                new_cmd :=cmd + "2018-05-21"
-                fmt.Printf(hour)
-                out := cmdExec(new_cmd)
-                ks := strings.Split(out, ",")
-                for _, v :=  range(ks){
-                  //if len(v)< 76{
-                      fmt.Printf(v)
-                  //}
-                } 
-         }
-       }
+
+func pullLog(date time.Time, cmd string, log chan string){
+    //hour := string(date.Hour())
+    dateString := date.Format("2006-01-02")
+    new_cmd :=cmd + dateString
+    out := cmdExec(new_cmd)
+    lens := len(out)
+    ks := strings.Split(out[1:lens-4], "  ")
+    for _,k := range(ks){ 
+       log <- k
     }
 }
 
+func work(cmd string, log chan string){
+    date := time.Now()
+    minute := int(date.Minute())
+    fmt.Println(minute) 
+    if (minute < 20) {
+        pullLog(date, cmd, log)
+    }else{
+        
+        ticker := time.NewTicker(3 * time.Second)
+        go func() {
+        for t := range ticker.C {
+            date := time.Now()
+            fmt.Println("\n",t)
+            pullLog(date, cmd, log)
+        }
+    }()
+    }
+}
+
+func init() {
+    runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 func main(){
      sigs := make(chan os.Signal, 1)  
      done := make(chan bool, 1)
+     log := make(chan string, 10)
      signal.Notify(sigs, os.Interrupt, os.Kill)
      go func() {  
         sig := <-sigs  
@@ -74,7 +88,8 @@ func main(){
         done <- true  
     }()
     cmd := "sudo radosgw-admin log list --date="
-    go pullLog(cmd)
+    go work(cmd, log)
+    go memcli(log)
     fmt.Println("awaiting signal") 
     <- done
     close(done)  

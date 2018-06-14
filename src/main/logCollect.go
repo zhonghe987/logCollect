3044c540a1b332f7ab0236a741c2e20629ddd66d
zhonghe987 <zhonghe987@163.com>
2018-06-14 16:07:21 +0800

    add sigs in order to exit

--- src/main/logCollect.go
+++ src/main/logCollect.go
@@ -3,7 +3,8 @@ package main
 import (
     "fmt"
     "os/exec"
-    _ "os"
+    "os"
+    "os/signal"
     "strings"
     "time"
     "bytes"
@@ -27,39 +28,55 @@ func cmdExec(cmd string) string {
 }
 
 
-func pullLog(date chan int, cmd string , sig chan int){
-    for {
+func pullLog(cmd string){
+     date := time.Now()
+     minute := int(date.Minute())
+     fmt.Println(minute) 
+     if (minute > 20) {
+         fmt.Printf("-------")
+     }else{
+       for {
+         
          select{
-            case <- date:
+            default:
                 now_date := time.Now()
                 hour := string(now_date.Hour())
-                dateString := now_date.Format("2006-01-02")
-                new_cmd :=cmd + dateString
+                //dateString := now_date.Format("2006-01-02")
+                new_cmd :=cmd + "2018-05-21"
                 fmt.Printf(hour)
                 out := cmdExec(new_cmd)
                 ks := strings.Split(out, ",")
                 for _, v :=  range(ks){
-                  if len(v)< 76{
-                      fmt.Printf("----"+v)
-                  }
+                  //if len(v)< 76{
+                      fmt.Printf(v)
+                  //}
                 } 
-                sig <- 1
-            default:
-                fmt.Println("exit")
          }
+       }
     }
 }
 
 
 func main(){
-     date := make(chan int, 1)
-     sig := make(chan int)
-     cmd := "sudo radosgw-admin log list --date="
-     go pullLog(date, cmd, sig)
-     //date_now := time.Now()
-     date <- 1
-     <-sig
-     defer close(date)
-     defer close(sig)
-     
+     sigs := make(chan os.Signal, 1)  
+     done := make(chan bool, 1)
+     signal.Notify(sigs, os.Interrupt, os.Kill)
+     go func() {  
+        sig := <-sigs  
+        switch sig {  
+        case os.Interrupt:  
+            fmt.Println("signal: Interrupt")  
+        case os.Kill:  
+            fmt.Println("signal: Kill")  
+        default:  
+            fmt.Println("signal: Others")  
+        }  
+        done <- true  
+    }()
+    cmd := "sudo radosgw-admin log list --date="
+    go pullLog(cmd)
+    fmt.Println("awaiting signal") 
+    <- done
+    close(done)  
+    fmt.Println("exiting")
 }
